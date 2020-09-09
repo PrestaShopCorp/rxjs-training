31f55c1726ec01250cdcf66192ad7675ce3a81f6
Clement Desiles <main@jokester.fr>
2020-09-09 10:14:06 +0200

    ex1

--- exercices/ex1.js
+++ exercices/ex1.js
@@ -1,4 +1,5 @@
-const {of} = require("rxjs");
+const { generate } = require("rxjs");
+const { filter } = require("rxjs/operators");
 
 /**
  * Exercice 1
@@ -8,10 +9,12 @@ const {of} = require("rxjs");
  * compris entre 100 et 5000 qui sont divisibles par 42.
  *
  */
-
 const ex1 = () => {
-    // TODO: Fix this function !
+  return generate(
+    0,
+    (x) => x < 5000,
+    (x) => x + 42
+  ).pipe(filter((n) => n > 100));
 };
 
-
-module.exports = {ex1};
+module.exports = { ex1 };
